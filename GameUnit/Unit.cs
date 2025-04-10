192ca411b95d6d6e76f704663de50d525c76b749
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-04-10 20:47:45 +0100

    Changes to Unit class

--- GameUnit/Unit.cs
+++ GameUnit/Unit.cs
@@ -1,14 +1,22 @@
+using System;
+using Humanizer;
 namespace GameUnit
 {
-    public class Unit
+    public abstract class Unit
     {
         private readonly int movement;
-        public int Health { get; set; }
+        public virtual int Health { get; set; }
+        public abstract float Cost { get; }
+
 
         public Unit(int movement, int health)
         {
             this.movement = movement;
             Health = health;
+        }
+        public void Move(int movement)
+        {
+            Console.WriteLine($"This unit moved {movement.ToRoman()}");
         }
     }
 }
