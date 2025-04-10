d080042689a97a3bd2c9a21b7775e28ce610c7d2
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-04-10 20:59:16 +0100

    Program.cs now tests 2 classes

--- GameUnit/Program.cs
+++ GameUnit/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿﻿using System;
+1 hour ago
+
+Add move, health and cost instances
+using System.Runtime.CompilerServices;
+2 hours ago
+
+Unit changes
 
 namespace GameUnit
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello LP1!");
+
+            MilitaryUnit a = new MilitaryUnit(10, 15, 20);
+            SettlerUnit b = new SettlerUnit();
+
+            b.Move(2);
+            a.Move(3);
+
+            Console.WriteLine($"The millitary unit has {a.Health} health and costs {a.Cost}");
+            Console.WriteLine($"The settler unit has {b.Health} health and costs {b.Cost}");
+
         }
     }
 }
